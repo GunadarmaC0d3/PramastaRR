5d503f9fd8c67f37b7f4caa606e2dc77b170c34c
PramastaRR <pramasta.ramadha@gmail.com>
2015-08-24 19:08:51 +0700

    Create latihan2.php

---
+++ belajar-php/latihan2.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            Belajar PHP
+        </title>
+    </head>
+    <body>
+        <?php
+            echo"<b>Selamat datang di website kami ^^</b><hr>"; //"<hr>" adalah perintah untuk membuat garis horizontal
+            echo"Hari ini tanggal ".date("d F Y");//"date("d F Y")" adalah perintah untuk menampilkan tanggal hari ini
+        ?>
+    </body>
+</html>
