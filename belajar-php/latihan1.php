e4c3336ba72d324f7bdac6b4381794112e7f69da
PramastaRR <pramasta.ramadha@gmail.com>
2015-08-24 19:06:56 +0700

    Create latihan1.php

---
+++ belajar-php/latihan1.php
@@ -0,0 +1,5 @@
+<?php
+    echo"Hai~<br>"; //ini komentar, tidak akan ditampilkan pada browser
+    echo"Ini dokumen PHP pertama saya~<br>";//"echo" adalah perintah untuk memunculkan tulisan
+    echo"Saya senang belajar PHP!"; //"<br>" adalah perintah untuk membuat baris baru
+?>
