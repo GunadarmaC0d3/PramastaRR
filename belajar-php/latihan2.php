71e4009e0a1358e2f82840be60bcefc4f3bc54e7
PramastaRR <pramasta.ramadha@gmail.com>
2015-08-25 20:12:57 +0700

    Create latihan2.php

---
+++ belajar-php/latihan2.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+      <title>
+            Belajar PHP
+        </title>
+    </head>
+    <body>
+        <?php
+            echo"<h1>Selamat datang di website kami</h1><hr>";
+            echo"Hari ini tanggal ".date("d F Y");
+        ?>
+    </body>
+</html>
